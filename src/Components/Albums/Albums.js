25d65dab2346839258463fc374dfa1b44a4e5013
Azlyum <Dyllanmarais@hotmail.com>
2021-12-19 20:04:37 +0000

    update

--- src/Components/Albums/Albums.js
+++ src/Components/Albums/Albums.js
@@ -11,22 +11,20 @@ const Albums = () => {
             width={150}
           />
           <div>
-            <h>Existence is Futile</h>
-            <br />
-            <l>
-              1. The Fate of the World on Our Shoulders <br />
-              2. Existential Terror <br />
-              3. Necromantic Fantasies <br />
-              4. Crawling King Chaos <br />
-              5. Here Comes a Candle... (Infernal Lullaby) <br />
-              6. Black Smoke Curling from the Lips of War <br />
-              7. Discourse Between a Man and His Soul <br />
-              8. The Dying of the Embers <br />
-              9. Ashen Mortality <br />
-              10. How Many Tears to Nurture a Rose? <br />
-              11. Suffer Our Dominion <br />
-              12. Us, Dark, Invincible
-            </l>
+            <h1>Existence is Futile</h1>
+            <h2>Name Of Songs From Album</h2>
+            1. The Fate of the World on Our Shoulders <br />
+            2. Existential Terror <br />
+            3. Necromantic Fantasies <br />
+            4. Crawling King Chaos <br />
+            5. Here Comes a Candle... (Infernal Lullaby) <br />
+            6. Black Smoke Curling from the Lips of War <br />
+            7. Discourse Between a Man and His Soul <br />
+            8. The Dying of the Embers <br />
+            9. Ashen Mortality <br />
+            10. How Many Tears to Nurture a Rose? <br />
+            11. Suffer Our Dominion <br />
+            12. Us, Dark, Invincible
           </div>
         </div>
       </div>
@@ -45,31 +43,29 @@ const Albums = () => {
             width={150}
           />
           <div>
-            <h>Live Bait For The Dead</h>
+            <h1>Live Bait For The Dead</h1>
+            <h2>Name Of Songs From Album</h2>
+            1. Intro – The Ceremony Opens <br />
+            2. Lord Abortion <br />
+            3. Ebony Dressed for Sunset <br />
+            4. The Forest Whispers My Name <br />
+            5. Cthulhu Dawn <br />
+            6. Dusk and Her Embrace <br />
+            7. The Principle of Evil Made Flesh <br />
+            8. Cruelty Brought Thee Orchids <br />v 9. Her Ghost in the Fog{" "}
             <br />
-            <l>
-              1. Intro – The Ceremony Opens <br />
-              2. Lord Abortion <br />
-              3. Ebony Dressed for Sunset <br />
-              4. The Forest Whispers My Name <br />
-              5. Cthulhu Dawn <br />
-              6. Dusk and Her Embrace <br />
-              7. The Principle of Evil Made Flesh <br />
-              8. Cruelty Brought Thee Orchids <br />v 9. Her Ghost in the Fog{" "}
-              <br />
-              10. Summer Dying Fast <br />
-              11. Interlude – Creatures That Kissed in Cold Mirrors <br />
-              12. From the Cradle to Enslave <br />
-              13. Queen of Winter, Throned <br />
-              14. Born in a Burial Gown (The Polished Coffin Mix) <br />
-              15. No Time to Cry (Sisters of No Mercy Mix) <br />
-              16. Funeral in Carpathia (Soundcheck Recording) <br />
-              17. Deleted Scenes of a Snuff Princess <br />
-              18. Scorched Earth Erotica (Original Demo(n) Version) <br />
-              19. Nocturnal Supremacy (Soundcheck Recording) <br />
-              20. From the Cradle to Enslave (Under Martian Rule Mix) <br />
-              21. The Fire Still Burns
-            </l>
+            10. Summer Dying Fast <br />
+            11. Interlude – Creatures That Kissed in Cold Mirrors <br />
+            12. From the Cradle to Enslave <br />
+            13. Queen of Winter, Throned <br />
+            14. Born in a Burial Gown (The Polished Coffin Mix) <br />
+            15. No Time to Cry (Sisters of No Mercy Mix) <br />
+            16. Funeral in Carpathia (Soundcheck Recording) <br />
+            17. Deleted Scenes of a Snuff Princess <br />
+            18. Scorched Earth Erotica (Original Demo(n) Version) <br />
+            19. Nocturnal Supremacy (Soundcheck Recording) <br />
+            20. From the Cradle to Enslave (Under Martian Rule Mix) <br />
+            21. The Fire Still Burns
           </div>
         </div>
       </div>
@@ -88,21 +84,19 @@ const Albums = () => {
             width={150}
           />
           <div>
-            <h>Midian</h>
-            <br />
-            <l>
-              1. At the Gates of Midian (Instrumental) <br />
-              2. Cthulhu Dawn <br />
-              3. Saffron's Curse <br />
-              4. Death Magick for Adepts <br />
-              5. Lord Abortion <br />
-              6. Amor e Morte <br />
-              7. Creatures That Kissed in Cold Mirrors (Instrumental) <br />
-              8. Her Ghost in the Fog <br />
-              9. Satanic Mantra <br />
-              10. Tearing the Veil from Grace <br />
-              11. Tortured Soul Asylum <br />
-            </l>
+            <h1>Midian</h1>
+            <h2>Name Of Songs From Album</h2>
+            1. At the Gates of Midian (Instrumental) <br />
+            2. Cthulhu Dawn <br />
+            3. Saffron's Curse <br />
+            4. Death Magick for Adepts <br />
+            5. Lord Abortion <br />
+            6. Amor e Morte <br />
+            7. Creatures That Kissed in Cold Mirrors (Instrumental) <br />
+            8. Her Ghost in the Fog <br />
+            9. Satanic Mantra <br />
+            10. Tearing the Veil from Grace <br />
+            11. Tortured Soul Asylum <br />
           </div>
         </div>
       </div>
